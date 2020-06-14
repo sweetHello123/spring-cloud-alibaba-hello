921b572911e20204678ea40d312670230b45bd01
china wu <18896820691@163.com>
2020-06-14 14:43:43 +0800

    init commit

---
+++ gateway/src/main/java/com/china/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.china.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: china wu
+ * @Description: 服务网关启动类
+ * @Date: 2020/6/14 13:59
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
